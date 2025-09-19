1847e038c30565e51f5a87653c9c65156aa61a3c
kartavya007 <84721894+kartavya007@users.noreply.github.com>
2025-09-19 18:23:42 +0000

    template_code

--- AIhelper.py
+++ AIhelper.py
@@ -1,12 +1,9 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAiBiJ13IeCAvXm5vo-RDamqUs8_utC8-M")
-model = genai.GenerativeModel('gemini-2.5-flash')
-response = model.generate_content(
-            [
-                """
-                hi
-                """
-            ]
-        )
-print(response.text) 
+
+class AiHelper:
+    def __init__(self):
+        genai.configure(api_key="AIzaSyAiBiJ13IeCAvXm5vo-RDamqUs8_utC8-M")
+        self.model = genai.GenerativeModel('gemini-2.5-flash')
+    
+    
